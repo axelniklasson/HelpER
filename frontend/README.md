e0001277fca611192e158ebca043dfc426d295bd
Axel Niklasson <axel.niklasson@live.com>
2018-05-16 07:40:03 +0200

    Added frontend boilerplate code

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# HelpER frontend
+
+## Development
+```
+yarn
+yarn start
+```
+
+## Production
+```
+yarn build
+```
---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HelpER boilerplate</h1>
+        <RaisedButton label="Hello world" />
+      </div>
+    );
+  }
+}
+
+export default App;
