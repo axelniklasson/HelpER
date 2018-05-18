eab7846fe7cfb4c34bbe4d5cc5da2527eb2a2452
Marcus Hultman <marcus.hultman1@gmail.com>
2018-05-18 22:01:35 +0200

    backend/server: static + fallback

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const fallback = require('express-history-api-fallback');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
 	res.json({ hello: 'world' });
 });
+
+const root = path.resolve(__dirname, '../frontend/build');
+
+app.use(express.static(root));
+app.use(fallback('index.html', { root }));
 
 app.listen(3000, () =>  {
 	console.log('HelpER backend running on port 3000');
