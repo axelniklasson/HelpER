a4d2b912f8ec81ce8ab5e31986b7a25583bde821
Axel Niklasson <axel.niklasson@live.com>
2018-05-19 08:35:35 +0200

    More boilerplate

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Header from './Header';
 import Component1 from './Component1';
 import Component2 from './Component2';
 import Component3 from './Component3';
+import Footer from './Footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props); 
+
+    // root state
+    this.state = {};
+  }
+
   render() {
     return (
       <Router>
         <div>
+          <Header />
           <Route exact path="/" component={Component1} />
           <Route path="/view2" component={Component2} />
           <Route path="/view3" component={Component3} />
+          <Footer />
         </div>
       </Router>
     );
