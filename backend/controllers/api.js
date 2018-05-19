15c1b51350991c5f9351ffcc94cda7e42486daf8
Johan Angséus <johan.angseus@gmail.com>
2018-05-19 09:36:31 +0200

    added router middleware logging

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,6 +1,11 @@
 const express   = require('express');
 const router    = express.Router();
 
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now(), ': ', req.baseUrl)
+  next()
+})
+
 router.route('/')
   .get(function(req, res, next) {
     res.redirect('/api/v1/status')
