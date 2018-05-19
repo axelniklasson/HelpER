6c6cdca72cfc8011c2dde4b85aaef60dde56e593
Johan Angséus <johan.angseus@gmail.com>
2018-05-19 09:39:46 +0200

    stats

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -1,19 +1,22 @@
 const express   = require('express');
 const router    = express.Router();
 
+var calls = 0;
+
 router.use(function (req, res, next) {
-  console.log('Time:', Date.now(), ': ', req.baseUrl)
-  next()
+  console.log('Time:', Date.now(), ', #', calls, ': ', req.baseUrl);
+  calls++;
+  next();
 })
 
 router.route('/')
   .get(function(req, res, next) {
-    res.redirect('/api/v1/status')
+    res.redirect('/api/v1/status');
   })
 
 router.route('/status')
   .get(function(req, res, next) {
-    res.json({'status':'running'})
+    res.json({'status':'running', 'requests':calls});
   })
 
 module.exports = router;
