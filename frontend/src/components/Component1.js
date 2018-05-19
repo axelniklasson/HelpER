6b8ed7b1c64f6c4d348ff7e25627f269af0dc81d
Marcus Hultman <marcus.hultman1@gmail.com>
2018-05-19 10:55:55 +0200

    frontend: add injury-types

--- frontend/src/components/Component1.js
+++ frontend/src/components/Component1.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 
-import Ankle from '../res/png/ankle.png';
-import Arm from '../res/png/arm.png';
-import Feet from '../res/png/feet.png';
-import Hand from '../res/png/hand.png';
-import Leg from '../res/png/leg.png';
-import Nose from '../res/png/nose.png';
-import Tongue from '../res/png/tongue.png';
+import * as injuryTypes from '../injury-types';
 
 import './Component1.scss';
 
@@ -24,6 +18,18 @@ class Component1 extends Component {
   componentDidMount = () => this.setState({ slideIn: true, slideOut: false })
   componentWillUnmount = () => this.setState({ slideIn: false, slideOut: true })
 
+  renderButtons() {
+    return Object.keys(injuryTypes).map(key => injuryTypes[key])
+    .map(({ id, name, alt, icon }) => (
+      <div className="injury-button" key={id}>
+        <div>
+          <img src={icon} alt={alt} />
+          <span>{name}</span>
+        </div>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div className={classNames(
@@ -31,48 +37,7 @@ class Component1 extends Component {
         this.state.slideIn ? 'slideIn' : '',
         this.state.slideOut ? 'slideOut' : ''
       )}>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(0)}>
-            <img src={Ankle} alt="ankle" />
-            <span>Ankles</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(1)}>
-            <img src={Arm} alt="arm" />
-            <span>Arm</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(2)}>
-            <img src={Feet} alt="feet" />
-            <span>Feet</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(3)}>
-            <img src={Hand} alt="hand" />
-            <span>Hand</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(4)}>
-            <img src={Leg} alt="leg" />
-            <span>Leg</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(5)}>
-            <img src={Nose} alt="nose" />
-            <span>Nose</span>
-          </div>
-        </div>
-        <div className="injury-button">
-          <div onClick={() => this.props.onClick(6)}>
-            <img src={Tongue} alt="tongue" />
-            <span>Tongue</span>
-          </div>
-        </div>
+        {this.renderButtons()}
       </div>
     ); 
   }
