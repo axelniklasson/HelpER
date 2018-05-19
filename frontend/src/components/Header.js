f4eaeb906dc28c4bb8f9dd55d4b75dc176ee1b0b
Marcus Hultman <marcus.hultman1@gmail.com>
2018-05-19 09:38:55 +0200

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import Logo from '../res/logo.png';
+
+import './Header.scss';
 
 class Header extends Component {
   render() {
     return (
-      <p>Header</p>
+      <div className="page-header">
+        <button className="header-button">foo</button>
+        <div className="text-center">
+          <div className="header-logo">
+            <img src={Logo}></img>
+            <div>HelpER</div>
+          </div>
+        </div>
+        <button className="header-button">bar</button>
+      </div>
     ); 
   }
 }
