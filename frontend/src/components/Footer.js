02e5a4a9e4213b2d2172dbfb6bd6365e5a7cd6ff
Marcus Hultman <marcus.hultman1@gmail.com>
2018-05-19 12:01:36 +0200

    frontend/Footer: update footer content

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <p>Footer</p>
+      <div className="text-center footer">
+        <a href="tel:112">If emergency, call 112</a>
+      </div>
     ); 
   }
 }
